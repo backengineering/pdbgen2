77c6ed433e1d4845a274b1c8359071377a8d7620
one-sec-omw-to-panda-express <PublicRelations@PandaRG.com>
2025-03-08 18:48:30 -0800

    im unable to generate a pdb that which x64dbg or IDA will display symbols from.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 // Copyright (C) Back Engineering Labs, Inc. - All Rights Reserved
 // https://www.youtube.com/watch?v=gxmXWXUvNr8
 
+#include <llvm/DebugInfo/CodeView/AppendingTypeTableBuilder.h>
+#include <llvm/DebugInfo/CodeView/ContinuationRecordBuilder.h>
 #include <llvm/DebugInfo/CodeView/StringsAndChecksums.h>
 #include <llvm/DebugInfo/CodeView/SymbolRecord.h>
+#include <llvm/DebugInfo/CodeView/SymbolSerializer.h>
 #include <llvm/DebugInfo/MSF/MSFBuilder.h>
 #include <llvm/DebugInfo/PDB/IPDBSession.h>
 #include <llvm/DebugInfo/PDB/Native/DbiModuleDescriptorBuilder.h>
@@ -15,6 +18,7 @@
 #include <llvm/DebugInfo/PDB/Native/PDBFile.h>
 #include <llvm/DebugInfo/PDB/Native/PDBFileBuilder.h>
 #include <llvm/DebugInfo/PDB/Native/SymbolStream.h>
+#include <llvm/DebugInfo/PDB/Native/TpiHashing.h>
 #include <llvm/DebugInfo/PDB/Native/TpiStream.h>
 #include <llvm/DebugInfo/PDB/Native/TpiStreamBuilder.h>
 #include <llvm/DebugInfo/PDB/PDB.h>
@@ -32,6 +36,15 @@ using namespace llvm;
 using namespace llvm::pdb;
 using namespace llvm::codeview;
 
+llvm::ExitOnError ExitOnErr;
+
+struct ModuleInfo {
+  std::vector<llvm::object::coff_section> sections;
+  llvm::codeview::GUID guid;
+  uint32_t age;
+  uint32_t signature;
+};
+
 static cl::opt<std::string>
     ObfuscatedPE("obf-pe", cl::desc("Path to the obfuscated PE file"),
                  cl::Required);
@@ -41,92 +54,110 @@ static cl::opt<std::string>
             cl::Required);
 
 static cl::opt<std::string>
-    OriginalPDB("orig-pdb", cl::desc("Path to the original PDB file"),
-                cl::Required);
-
-static cl::opt<std::string>
     OutputPDB("out-pdb", cl::desc("Path to the output PDB file"), cl::Required);
+
+// https://github.com/gix/PdbGen/blob/568d23b671eda39d7bc562e511e8dda4b18aa18b/Main.cpp#L37
+llvm::Error ReadModuleInfo(llvm::StringRef modulePath, ModuleInfo &info) {
+  using namespace llvm;
+  using namespace llvm::object;
+
+  auto expectedBinary = createBinary(modulePath);
+  if (!expectedBinary)
+    return expectedBinary.takeError();
+
+  OwningBinary<Binary> binary = std::move(*expectedBinary);
+
+  if (binary.getBinary()->isCOFF()) {
+    auto const obj = llvm::cast<COFFObjectFile>(binary.getBinary());
+    for (auto const &sectionRef : obj->sections())
+      info.sections.push_back(*obj->getCOFFSection(sectionRef));
+
+    for (auto const &debugDir : obj->debug_directories()) {
+      info.signature = debugDir.TimeDateStamp;
+      if (debugDir.Type == IMAGE_DEBUG_TYPE_CODEVIEW) {
+        DebugInfo const *debugInfo;
+        StringRef pdbFileName;
+        ExitOnErr(obj->getDebugPDBInfo(&debugDir, debugInfo, pdbFileName));
+
+        switch (debugInfo->Signature.CVSignature) {
+        case OMF::Signature::PDB70:
+          info.age = debugInfo->PDB70.Age;
+          std::memcpy(&info.guid, debugInfo->PDB70.Signature,
+                      sizeof(info.guid));
+          break;
+        }
+      }
+    }
+
+    return Error::success();
+  }
+
+  return errorCodeToError(std::make_error_code(std::errc::not_supported));
+}
 
 int main(int argc, char **argv) {
   cl::ParseCommandLineOptions(
       argc, argv,
       "CodeDefender PDB Generator\n- Made with love by CR3Swapper :)\n");
 
-  llvm::ExitOnError ExitOnErr;
   llvm::BumpPtrAllocator alloc;
   llvm::pdb::PDBFileBuilder builder(alloc);
-  const auto moduleName = "CodeDefender.obj";
+  ModuleInfo moduleInfo;
 
-  // Read the file off the disk and create a PDBFile.
-  std::unique_ptr<IPDBSession> session;
-  ExitOnErr(loadDataForPDB(PDB_ReaderType::Native, OriginalPDB, session));
-  NativeSession *NS = static_cast<NativeSession *>(session.get());
-  auto &pdb = NS->getPDBFile();
-
-  ExitOnErr(builder.initialize(pdb.getBlockSize()));
+  ExitOnErr(ReadModuleInfo(ObfuscatedPE, moduleInfo));
+  ExitOnErr(builder.initialize(4096));
 
   // Add each of the reserved streams.  We might not put any data in them,
   // but at least they have to be present.
   for (uint32_t i = 0; i < llvm::pdb::kSpecialStreamCount; ++i)
     ExitOnErr(builder.getMsfBuilder().addStream(0));
 
-  InfoStream &info = pdb.getPDBInfoStream().get();
   InfoStreamBuilder &infoBuilder = builder.getInfoBuilder();
-
-  infoBuilder.setVersion(info.getVersion());
-  infoBuilder.setSignature(info.getSignature());
-  infoBuilder.setAge(info.getAge());
-  infoBuilder.setGuid(info.getGuid());
+  infoBuilder.setSignature(moduleInfo.signature);
+  infoBuilder.setAge(moduleInfo.age);
+  infoBuilder.setGuid(moduleInfo.guid);
+  infoBuilder.setHashPDBContentsToGUID(false);
+  infoBuilder.setVersion(llvm::pdb::PdbImplVC70);
   infoBuilder.addFeature(llvm::pdb::PdbRaw_FeatureSig::VC140);
 
-  DbiStream &dbi = pdb.getPDBDbiStream().get();
   DbiStreamBuilder &dbiBuilder = builder.getDbiBuilder();
   llvm::ArrayRef<llvm::object::coff_section> sections(
-      std::vector<llvm::object::coff_section>{dbi.getSectionHeaders().begin(),
-                                              dbi.getSectionHeaders().end()});
+      std::vector<llvm::object::coff_section>{moduleInfo.sections.begin(),
+                                              moduleInfo.sections.end()});
 
-  dbiBuilder.setAge(dbi.getAge());
-  dbiBuilder.setBuildNumber(dbi.getBuildNumber());
-  dbiBuilder.setFlags(dbi.getFlags());
-  dbiBuilder.setMachineType(dbi.getMachineType());
+  dbiBuilder.setAge(moduleInfo.age);
+  dbiBuilder.setBuildNumber(14, 11);
+  dbiBuilder.setMachineType(PDB_Machine::Amd64);
   dbiBuilder.setPdbDllRbld(1);
   dbiBuilder.setPdbDllVersion(1);
   dbiBuilder.setVersionHeader(PdbDbiV70);
+  dbiBuilder.setFlags(DbiFlags::FlagStrippedMask);
   dbiBuilder.createSectionMap(sections);
+
+  auto &tpiBuilder = builder.getTpiBuilder();
+  tpiBuilder.setVersionHeader(llvm::pdb::PdbTpiV80);
+
+  auto &ipiBuilder = builder.getIpiBuilder();
+  ipiBuilder.setVersionHeader(llvm::pdb::PdbTpiV80);
+
   ExitOnErr(dbiBuilder.addDbgStream(
       DbgHeaderType::SectionHdr,
       {reinterpret_cast<uint8_t const *>(sections.data()),
        sections.size() * sizeof(sections[0])}));
 
-  // Create a new single module named "CodeDefender.obj"
-  auto &modiBuilder = ExitOnErr(dbiBuilder.addModuleInfo(moduleName));
-  modiBuilder.setObjFileName(moduleName);
+  auto &modiBuilder =
+      ExitOnErr(dbiBuilder.addModuleInfo("C:\\CodeDefender.obj"));
+  modiBuilder.setObjFileName("C:\\CodeDefender.obj");
+  auto &gsiBuilder = builder.getGsiBuilder();
 
-  // Copy types used from the original binary?
-  TpiStream &tpi = pdb.getPDBTpiStream().get();
-  TpiStreamBuilder &tpiBuilder = builder.getTpiBuilder();
-  tpiBuilder.setVersionHeader(llvm::pdb::PdbTpiV80);
-  for (auto idx = tpi.TypeIndexBegin(); idx != tpi.TypeIndexEnd(); idx++) {
-    CVType ty = tpi.getType(TypeIndex(idx));
-    tpiBuilder.addTypeRecord(ty.RecordData, std::nullopt);
-  }
+  ProcSym proc = ProcSym(SymbolRecordKind::GlobalProcSym);
+  proc.Name = "HelloWorldTest";
+  proc.Segment = 0;
+  proc.CodeOffset = 0;
+  proc.CodeSize = 5;
 
-  // Copy types from the original binary?
-  TpiStream &ipi = pdb.getPDBIpiStream().get();
-  auto &ipiBuilder = builder.getIpiBuilder();
-  ipiBuilder.setVersionHeader(llvm::pdb::PdbTpiV80);
-  for (auto idx = ipi.TypeIndexBegin(); idx != ipi.TypeIndexEnd(); idx++) {
-    CVType ty = ipi.getType(TypeIndex(idx));
-    ipiBuilder.addTypeRecord(ty.RecordData, std::nullopt);
-  }
-
-  GSIStreamBuilder &gsiBuilder = builder.getGsiBuilder();
-  SymbolStream &symstream = pdb.getPDBSymbolStream().get();
-
-  bool hadError = false;
-  for (auto &sym : symstream.getSymbols(&hadError)) {
-    gsiBuilder.addGlobalSymbol(sym);
-  }
+  modiBuilder.addSymbol(
+      SymbolSerializer::writeOneSymbol(proc, alloc, CodeViewContainer::Pdb));
 
   codeview::GUID ignored;
   ExitOnErr(builder.commit(OutputPDB, &ignored));
